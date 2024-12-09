83611ac45e84357d2bf929abd2aafebc18020c1a
jgocm <jgocm@cin.ufpe.br>
2024-12-09 15:04:56 -0300

    wip

--- dataset_analyzer.py
+++ dataset_analyzer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-annotations_path = 'annotations/segments_annotations.csv'
+annotations_path = 'annotations/ssl-dataset/segments_annotations.csv'
 df = pd.read_csv(annotations_path)
 
 true_counts = df[['is_field_boundary', 'is_field_marking', 'is_not_a_field_feature']].sum()
